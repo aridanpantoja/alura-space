5bbdc99e51f5176f96dbfa371490e40a8a7cb3aa
Aridan Pantoja <aridanpantoja@gmail.com>
2024-12-13 10:49:33 -0300

    chore: configure jest and testing library

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jsdom',
+}
+
+export default createJestConfig(config)
